1f04a2bf9f66a691bf1dd2bd757c32c3115a15f3
Zsolnai Krisztián Viktor <zsolnai.krisztian@trefortszki.hu>
2021-10-14 10:09:57 +0200

    gomb.ino updated

---
+++ gomb/gomb.ino
@@ -0,0 +1,24 @@
+int gomb = 3;
+void setup() {
+ Serial.begin(9600);
+ pinMode(3,INPUT_PULLUP);
+ 
+
+}
+
+void loop() {
+ 
+//Serial.println(digitalRead(3));
+
+if(digitalRead(3)==0)
+{
+  Serial.println("megnyomtak! Udv a gomb!");
+  delay(100);
+  }else
+  {
+    Serial.println("nem vagyok megnyomva");
+    delay(1000);
+    }
+  
+
+}
